ef0cc95b059da3d5a82a02dd82852af43dc6e2be
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-01-04 21:24:38 +0100

    Bug fixed in tagContent (regarding edit buttons)

--- src/components/tag/tagContent.tsx
+++ src/components/tag/tagContent.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 const ButtonTagClassname = 'w-8 h-8 p-1 rounded-lg text-[#686868] hover:bg-black/10 transition-all';
 
-const Content = ({ tag, modifiable }: Props) => {
+const Content = ({ tag, modifiable, hovering }: Props) => {
     const { openModal } = useContext(ModalContext)!;
 
     const handleDelete = () => {
@@ -42,17 +42,26 @@ const Content = ({ tag, modifiable }: Props) => {
             <p className='w-[calc(100%-64px)] break-words'>{tag.name}</p>
             {modifiable && (
                 <div className='z-10 flex w-12 items-center gap-1 pl-1'>
-                    <button
-                        id='little-tag-btn-1'
-                        onMouseEnter={() => handleHover('1')}
-                        onMouseLeave={() => handleClose('1')}
-                        className={ButtonTagClassname + ' !right-12'}
-                        onClick={handleEdit}>
-                        <MdModeEdit className='h-6 w-6' />
-                    </button>
-                    <button id='little-tag-btn-2' onMouseEnter={() => handleHover('2')} onMouseLeave={() => handleClose('2')} className={ButtonTagClassname} onClick={handleDelete}>
-                        <IoClose className='h-6 w-6' />
-                    </button>
+                    {hovering && (
+                        <>
+                            <button
+                                id='little-tag-btn-1'
+                                onMouseEnter={() => handleHover('1')}
+                                onMouseLeave={() => handleClose('1')}
+                                className={ButtonTagClassname + ' !right-12'}
+                                onClick={handleEdit}>
+                                <MdModeEdit className='h-6 w-6' />
+                            </button>
+                            <button
+                                id='little-tag-btn-2'
+                                onMouseEnter={() => handleHover('2')}
+                                onMouseLeave={() => handleClose('2')}
+                                className={ButtonTagClassname}
+                                onClick={handleDelete}>
+                                <IoClose className='h-6 w-6' />
+                            </button>
+                        </>
+                    )}
                 </div>
             )}
         </div>
