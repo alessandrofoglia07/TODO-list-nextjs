cc31b00833c4c6fcaf53ea07fa4c98775b5a31c5
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-01-07 15:46:17 +0100

    Added reset search button to mainpage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,6 +6,7 @@ import { db } from '@/lib/db';
 import { getQuery } from '@/lib/getQuery';
 import { initialProfile } from '@/lib/initialProfile';
 import { SearchParams } from '@/types';
+import Link from 'next/link';
 
 interface Props {
     searchParams?: SearchParams;
@@ -62,7 +63,14 @@ const MainPage = async ({ searchParams }: Props) => {
     return (
         <div className='px-6 py-8'>
             <h1 className='mt-4 text-5xl font-bold'>Todo Wall</h1>
-            <div className='mt-2 flex flex-col'>{await getSubtitle()}</div>
+            <div className='flex w-full items-center justify-between'>
+                <div className='mt-2 flex flex-col'>{await getSubtitle()}</div>
+                {searchParams && (searchParams.tag || searchParams.list) && (
+                    <Link href='/' className='shadow-hover rounded-xl border-2 border-black/20 bg-red-400 px-4 py-2 text-white'>
+                        Reset Search
+                    </Link>
+                )}
+            </div>
             <div id='todos' className='flex h-full w-full flex-wrap gap-6 px-8 py-16'>
                 {todos.map((todo) => (
                     <Todo key={todo.id} todo={todo} />
